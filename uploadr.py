8be2fd8dedc35135d5c9efc0ada403d660a9fe83
oPromessa <mario@silvapereira.com>
2018-01-15 23:23:35 +0100

    debug bad loaded files on flickrapi 8

--- uploadr.py
+++ uploadr.py
@@ -86,12 +86,12 @@ class UPLDRConstants:
 
     # -------------------------------------------------------------------------
     # Color Codes for colorful output
-    W = '\033[0m'    # white (normal)
-    R = '\033[31m'   # red
-    G = '\033[32m'   # green
-    O = '\033[33m'   # orange
-    B = '\033[34m'   # blue
-    P = '\033[35m'   # purple
+    W = ''   # white (normal)
+    R = ''   # red
+    G = ''   # green
+    O = ''   # orange
+    B = ''   # blue
+    P = ''   # purple
 
     # -------------------------------------------------------------------------
     # class UPLDRConstants __init__

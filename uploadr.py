4b898f010236619836b7d1a79ad50be7c6a22dce
oPromessa <mario@silvapereira.com>
2018-02-24 14:30:35 +0100

    output fix 2

--- uploadr.py
+++ uploadr.py
@@ -1565,8 +1565,8 @@ class Uploadr:
             # CODING
             # For Debugging: UnicodeWarning comparison 
             logging.info('Check for UnicodeWarning comparison dirpath:[{!s}]'
-                         .StrUnicodeOut(os.path.basename(
-                                                os.path.normpath(dirpath))))
+                         .format(StrUnicodeOut(os.path.basename(
+                                                os.path.normpath(dirpath)))))
             if StrUnicodeOut(os.path.basename(os.path.normpath(dirpath))) \
                 in EXCLUDED_FOLDERS:
                 dirnames[:] = []

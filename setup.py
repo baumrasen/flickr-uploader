36b344d6103c3215b7d63afec14e73e3c2d6858b
oPromessa <mario@silvapereira.com>
2018-03-12 23:17:15 +0100

    App setup package...

--- setup.py
+++ setup.py
@@ -2,12 +2,31 @@
 
 from distutils.core import setup
 
-setup(name='Flickr-Uploader',
+setup(
+      # Application name:
+      name='Flickr-Uploader',
+
+      # Version number (initial):
       version='2.7.1',
       description='Python Distribution Utilities',
+
+      # Application author details:
       author='oPromessa',
       author_email='oPromessa@github.com',
       url='https://github.com/oPromessa/flickr-uploader/',
-      packages=['uploadr'],
-     )
 
+      # Packages
+      packages=['uploadr'],
+
+      # Include additional files into the package
+      #include_package_data=True,
+
+      # license="LICENSE.txt",
+
+      # long_description=open("README.md").read(),
+
+      # Dependent packages (distributions)
+      install_requires=[
+          "flickrapi",
+      ],
+)

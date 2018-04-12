c74964be6a21095d6830cf972caba3c487b9ba06
oPromessa <mario@silvapereira.com>
2018-04-12 19:37:13 +0200

    typo

--- lib/myconfig.py
+++ lib/myconfig.py
@@ -346,10 +346,10 @@ class MyConfig(object):
             if not os.path.isdir(os.path.dirname(self.__dict__[item])):
                 logging.critical('{!s}:[{!s}] is not in a valid folder:[{!s}].'
                                  .format(item,
-                                         StrUnicodeOut(self.__dict__[item])),
+                                         StrUnicodeOut(self.__dict__[item]),
                                          StrUnicodeOut(os.path.dirname(
                                              self.__dict__[item]))
-                                )
+                                ))
 
         # Further specific processing... EXCLUDED_FOLDERS
         #     Read EXCLUDED_FOLDERS and convert them into Unicode folders

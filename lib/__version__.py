b7fc2f7d2bace10a4e1dd181fc1fa0f01c537e21
oPromessa <mario@silvapereira.com>
2018-05-22 23:41:37 +0200

    1st PyPi compatible version
    - update setup.py
    - setup.py upload option
    - test copy data files to etc folder

--- lib/__version__.py
+++ lib/__version__.py
@@ -5,6 +5,6 @@
     __version__ = Semantic Versioning number Major.Minor.Patch
                   Check https://semver.org
 """
-VERSION = (2, 8, 5)
+VERSION = (2, 8, '5+a')
 
 __version__ = '.'.join(map(str, VERSION))

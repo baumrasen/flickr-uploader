dcc855bace14f82166cf1e540f04ea56c4440d83
oPromessa <mario@silvapereira.com>
2018-09-18 23:30:21 +0200

    _hashrepl removed encode.utf

--- lib/NicePrint.py
+++ lib/NicePrint.py
@@ -327,9 +327,7 @@ class RedactingFormatter(logging.Formatter):
     def _hashrepl(self, matchobj):
         # print('>in  matchobj:[{!s}]/type:[{!s}]'
         #       .format(matchobj.group(0), type(matchobj.group(0))))
-        _hexmatch = '>' +\
-            hashlib.sha1(matchobj.group(0).encode('utf-8')).hexdigest() +\
-            '<'
+        _hexmatch = '>' + hashlib.sha1(matchobj.group(0)).hexdigest() + '<'
         # print('>out matchobj:[{!s}]/type:[{!s}]'
         #       .format(_hexmatch, type(matchobj.group(0))))
         return _hexmatch
